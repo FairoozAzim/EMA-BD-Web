5aa71845170e2e9931a438da93180af6a2eff787
Sadad Mahamud <sadad.mahamud@aalto.fi>
2024-10-01 11:37:16 +0300

    EUD Infor updated and Picture size

--- ema-bd-website/src/pages/AboutPage/EUD_dets.jsx
+++ ema-bd-website/src/pages/AboutPage/EUD_dets.jsx
@@ -6,7 +6,7 @@ export default [
     
         {
             "name" : "H.E Michael Miller",
-            "year" : "2024",
+            "year" : "2024 - Present",
             "keynote": "Coming Soon!",
             "image": Michael
         
@@ -14,7 +14,7 @@ export default [
         },
         {
             "name" : "Charles Witeley",
-            "year" : "202",
+            "year" : "2021 - 2024",
             "highlight": "Dear members of the Erasmus Mundus Association-Bangladesh (EMA-BD),",
             "keynote": "It is with great pleasure and enthusiasm that I address you today on the transformative power of Erasmus+ educational opportunities and cross-cultural exchange. Erasmus Mundus has long been a beacon of academic excellence and cultural diversity, fostering mutual understanding and collaboration across borders. As we navigate a rapidly changing global landscape, the importance of international education and intercultural dialogue cannot be overstated. These experiences not only enrich individual lives but also strengthen the foundation of our societies by promoting tolerance, empathy, and global citizenship.\nThe Erasmus+ programme embodies the European Union&quot;s commitment to foster educational mobility and enhancing cooperation between institutions worldwide. It provstudents, academics, and professionals with invaluable opportunities to study, teach,collaborate in diverse cultural settings, thereby broadening their perspectives and prepathem to tackle global challenges. \nFor students and professionals in Bangladesh, engaging with Erasmus+ opens door academic excellence, innovative research, and personal growth. It offers a platform to exch ideas, knowledge, and best practices with peers from different backgrounds, laying groundwork for future leaders and change-mak As we look ahead, I encourage each of you to seize the opportunities that Erasmus Mu presents. Embrace the chance to broaden your horizons, challenge your assumptions, and f lasting connections across continents. Your participation not only enhances your own ca prospects but also contributes to building a more interconnected and prosperous w I commend the Erasmus Mundus Association-Bangladesh for its dedication to promo educational excellence and international cooperation. Your commitment to advan Erasmus+ and fostering a vibrant community of learners and leaders. Furthermore, the la of the EMA-BD website will serve as a pivotal platform for connecting our community, sha resources, and expanding opportunities for academic and cultural exchange. \nTogether, let us celebrate the achievements of Erasmus Mundus and rededicate ourselve building a brighter future through education, understanding, and shared huma Thank you for your commitment to Erasmus+, and I look forward to witnessing the conti success and impact of EMA-BD. \nThanks\nCharles Whiteley\nAmbassador and Head of Delegation\nEuropean Union Delegation to Bangladesh",
             "image": Charles
